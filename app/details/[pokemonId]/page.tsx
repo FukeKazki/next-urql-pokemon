9b8043f6a3bd0df33b6fe79e44fcc0a9172c3db0
FukeKazki <kazkichi0906@gmail.com>
2023-11-23 23:48:18 +0900

    詳細ページも

--- app/details/[pokemonId]/page.tsx
+++ app/details/[pokemonId]/page.tsx
@@ -1,9 +1,17 @@
-import Link from "next/link";
+import { PokemonModal } from "@/app/@modal/(.)details/[pokemonId]/modal";
+import { GetPokemon } from "@/graphql/query";
+import { getClient } from "@/lib/urql";
 
-export default function Page() {
-  return (
-    <>
-      this is details <Link href="/">topへ</Link>
-    </>
-  );
+type Props = {
+  params: {
+    pokemonId: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
+  const result = await getClient().query(GetPokemon, {
+    id: decodeURIComponent(params.pokemonId),
+  });
+
+  return <PokemonModal pokemon={result.data?.pokemon} />;
 }
